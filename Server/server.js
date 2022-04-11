054bc855d7767360bf0e9e429f3fa936eff8b1c0
Lazar Bogdan <lazarbogdan@Lazars-MacBook-Air.local>
2022-04-11 13:50:13 +0300

    server-new-setup

--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,13 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname,'..','public');
+const publicPath = path.join(__dirname,"../Client/build");
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
 app.get('*', (req,res) =>{
-	res.sendFile(path.join(publicPath,'/src/index.js'));
+	res.sendFile(path.join(publicPath,'index.js'));
 });
 
 app.listen(port, () =>{
