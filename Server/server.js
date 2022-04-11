6a96cedb4054c39ac054906c56f9ae754ed035cf
Lazar Bogdan <lazarbogdan@Lazars-MacBook-Air.local>
2022-04-11 13:56:53 +0300

    second try

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname,"../Client/build");
+const publicPath = path.join(__dirname,"../build");
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
