55773c40ef9a98bf271d42e76b8979cb67ad92e7
Lazar Bogdan <lazarbogdan@Lazars-MacBook-Air.local>
2022-04-11 11:41:08 +0300

    this is a test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit this file to have something  <code>src/App.js</code> this is a test.
+          This is a test  <code>src/App.js</code> this is a test.
         </p>
         <a
           className="App-link"
