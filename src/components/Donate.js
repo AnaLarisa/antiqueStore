5db9a51a1e7baa6ff651fd5edf48137443096394
Lavinia-CristinaRus <lavinia.rus20@gmail.com>
2022-05-02 00:21:16 +0300

    donate page

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -3,6 +3,7 @@ import 'react-router-dom'
 import {Link} from 'react-router-dom';
 import './CSS/Home.css'; 
 import "./CSS/Navbar.css";
+import "./CSS/Donate.css";
 
 function Donate() {
     const [isOpen, setIsOpen] = useState(false);
@@ -27,7 +28,43 @@ function Donate() {
                 </div>
             </div>
             <div className={`${isOpen && "hide"}`}>
-                <h1>Donate here </h1>
+                <section class = "banner">
+                    <h2>SCHEDULE THE DONATION</h2>
+                    <div class = "card-container">
+                        <div class = "card-img">
+                        </div>
+
+                        <div class = "card-content">
+                            <h3>Schedule</h3>
+                            <form>
+                                    <select name = "days" className="donate">
+                                        <option value = "day-select">Select Day</option>
+                                        <option value = "sunday">Sunday</option>
+                                        <option value = "monday">Monday</option>
+                                        <option value = "tuesday">Tuesday</option>
+                                        <option value = "wednesday">Wednesday</option>
+                                        <option value = "thursday">Thursday</option>
+                                        <option value = "friday">Friday</option>
+                                        <option value = "saturday">Saturday</option>
+                                    </select>
+
+                                    <select name = "hours" className="donate">
+                                        <option value = "hour-select">Select Hour</option>
+                                        <option value = "10">10: 00</option>
+                                        <option value = "10">12: 00</option>
+                                        <option value = "10">14: 00</option>
+                                        <option value = "10">16: 00</option>
+                                        <option value = "10">18: 00</option>
+                                        <option value = "10">20: 00</option>
+                                        <option value = "10">22: 00</option>
+                                    </select>
+                                    <input type = "text" className="donate" placeholder="Phone Number"/>
+                                    <input type = "number" className="donate" placeholder="Number of Books" min = "1"/>
+                                    <input type = "submit" className="donate" value = "SCHEDULE"/>
+                            </form>
+                        </div>
+                    </div>
+                </section>
             </div>
             </div>
         </div>
