893d22ae4f879a27b9d45bc48c79e2227bcd60f4
dianachismore <diana.chismore@gmail.com>
2022-05-04 13:49:58 +0300

    Syncfusion warning resolved + navbar color changed

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -6,8 +6,8 @@ const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOWE0MDBmOTdmOTg3
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
 
-const user = JSON.parse(localStorage.getItem("persist:root")).user;
-const currentUser = user && JSON.parse(user).currentUser;
+//const user = JSON.parse(localStorage.getItem("persist:root")).user;
+//const currentUser = user && JSON.parse(user).currentUser;
 //const TOKEN = currentUser?.accessToken;
 
 export const publicRequest = axios.create({
