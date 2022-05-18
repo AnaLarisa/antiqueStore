5b749d91413b718da67b0ac422356447df6d9e86
dianachismore <diana.chismore@gmail.com>
2022-05-19 00:04:05 +0300

    search + wrap books

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,34 @@ import './CSS/Home.css';
 import "./CSS/Navbar.css";
 import {Widget} from 'react-chat-widget';
 import './CSS/styles.css'
-import MainContent from "./MainContent.js";
 import './CSS/HomePage.css';
+import { AiOutlineSearch } from "react-icons/ai";
+import product_card from "./product_data.js"
+
 
 function Home() {
     const [isOpen, setIsOpen] = useState(false);
+    const [searchTerm,setSearchTerm]=useState("");
+    const Books = product_card.filter((item) => {
+        if(searchTerm ==""){
+            return item;
+        }
+        else if(item.product_name.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return item;
+        }
+    })
+    const Filtered = Books.map((item) =>
+        <div className="card" key={item.id}>
+            <div className="card_img">
+                <img src={require('./images/' + item.image +'.png')} />
+            </div>
+            <div className="card_header">
+                <h2>{item.product_name}</h2>
+                <p className="price">{item.price}<span>{item.currency}</span></p>
+                <div className="btn">Add to cart</div>
+            </div>
+        </div>
+     );
     return (
         <div className="under">
             <div className="over">
@@ -17,6 +40,17 @@ function Home() {
                 <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Irish+Grover" />
                 <span className="nav-logo"><span>ANTIQUE</span><br/>
                 <span>STORE</span></span>
+                <div className="wrapper">
+                    <AiOutlineSearch
+                    style={{
+                        position: 'absolute',
+                        top: '8px',
+                        left: '8px',
+                        width: '14px',
+                      }}
+                    />
+                    <input className="search" placeholder="Search" onChange={(event) => {const { target } = event; setTimeout(() => {setSearchTerm(target.value);}, 500)}}/>
+                </div>
                 <div className={`nav-items ${isOpen && "open"}`}>
                     <Link to ="/donate">Donate</Link>
                     <Link to="/mycart">My Cart</Link>
@@ -32,8 +66,8 @@ function Home() {
                     <div className="bar"></div>
                 </div>
             </div>
-            <div className={`${isOpen && "hide"}`}>
-                    <MainContent />
+            <div className={`${(isOpen && "hide") || (!isOpen && "homeContent")}`}>
+                    {Filtered}
                 </div>
                 <div className='App'>
                     <Widget
